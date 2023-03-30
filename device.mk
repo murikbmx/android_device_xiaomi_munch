aa49ce5eff596eb81bbd89912c8d2665739c110c
Vincent4440 <aditya.singh2008v@gmail.com>
2023-03-30 13:03:12 +0530

    munch: Include GcamGO
    Signed-off-by: Vincent4440 <aditya.singh2008v@gmail.com>

--- device.mk
+++ device.mk
@@ -10,6 +10,10 @@ TARGET_IS_VAB := true
 # Inherit from sm8250-common
 $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 
+# Camera
+PRODUCT_PACKAGES += \
+    GoogleCameraGo
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
